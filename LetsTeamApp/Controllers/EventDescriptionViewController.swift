449d31b05d5fe4f23ddb4787a1443ddf5da2965d
Misha <misha.lazar90@gmail.com>
2018-08-04 21:52:13 +0100

    card view event description view

---
+++ LetsTeamApp/Controllers/EventDescriptionViewController.swift
@@ -0,0 +1,25 @@
+//
+//  EventDescriptionViewController.swift
+//  LetsTeamApp
+//
+//  Created by admin on 8/4/18.
+//  Copyright © 2018 admin. All rights reserved.
+//
+
+import UIKit
+
+class EventDescriptionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
